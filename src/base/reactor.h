3f02221f7e983d15eebc38a88ebb50bb6f94eff5
mingz <305603665@qq.com>
2017-04-24 18:43:55 +0800

    base

--- src/base/reactor.h
+++ src/base/reactor.h
@@ -5,5 +5,17 @@
 #ifndef SERVER_BASE_C_REACTOR_H
 #define SERVER_BASE_C_REACTOR_H
 
+struct Reactor {
+
+};
+
+static struct Reactor *reactor_new();
+
+static void add_accepter();
+
+static void add_connector();
+
+static void run(char *ip, int port);
+
 
 #endif //SERVER_BASE_C_REACTOR_H
