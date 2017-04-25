97985749abce4611fa4245b51ecb23baeb1b29a2
mingz <305603665@qq.com>
2017-04-25 16:52:32 +0800

    init

--- src/socket.h
+++ src/socket.h
@@ -6,4 +6,25 @@
 #define SERVER_BASE_C_SOCKET_H
 
 
+void socket_nonblocking();
+
+void socket_close();
+
+void socket_connect();
+
+void socket_accept();
+
+void socket_listen();
+
+void socket_open();
+
+void socket_send();
+
+void socket_bind();
+
+void socket_new();
+
+void socket_free();
+
+
 #endif //SERVER_BASE_C_SOCKET_H
